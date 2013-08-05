46d08e97c6b9e04c86ecc6c03760ff152c9c0062
Chris Yeoh <cyeoh@au1.ibm.com>
2013-08-05 16:28:41 +0930

    WIP: Create metafiles for API v3 samples
    Create metafiles for the API v3 samples which can be used to
    help automate the generation of specification documentation for
    the v3 API
    
    Adds the infrastructure for creating the meta files plus adds
    the changes for hosts extension
    
    Partially implements blueprint v3-api-specification
    
    Change-Id: Ie443931ea6bb49fbf2848f2223d0e47436005c8f

--- nova/tests/integrated/v3/api_sample_base.py
+++ nova/tests/integrated/v3/api_sample_base.py
@@ -18,6 +18,7 @@ import os
 from oslo.config import cfg
 
 from nova.api.openstack import API_V3_CORE_EXTENSIONS  # noqa
+from nova.openstack.common import jsonutils
 from nova import test
 from nova.tests import fake_network
 from nova.tests import fake_utils
@@ -30,6 +31,8 @@ class ApiSampleTestBaseV3(api_samples_test_base.ApiSampleTestBase):
     _api_version = 'v3'
     sample_dir = None
     extra_extensions_to_load = None
+    section_name = None
+    section_doc = None
 
     def setUp(self):
         extends = []
@@ -54,6 +57,7 @@ class ApiSampleTestBaseV3(api_samples_test_base.ApiSampleTestBase):
         fake_network.stub_compute_with_ips(self.stubs)
         fake_utils.stub_out_utils_spawn_n(self.stubs)
         self.generate_samples = os.getenv('GENERATE_SAMPLES') is not None
+        self.metainfo = {}
 
     @classmethod
     def _get_sample_path(cls, name, dirname, suffix=''):
@@ -78,3 +82,77 @@ class ApiSampleTestBaseV3(api_samples_test_base.ApiSampleTestBase):
     def _get_template(cls, name):
         dirname = os.path.dirname(os.path.abspath(__file__))
         return cls._get_sample_path(name, dirname, suffix='.tpl')
+
+    def _doc_get_response(self, url, method, body=None, strip_version=False,
+                           **kwargs):
+        response = self._get_response(url, method, body, strip_version)
+        if 'api_desc' in kwargs:
+            self.metainfo['section_name'] = self.section_name
+            self.metainfo['extension'] = self.extension_name
+            self.metainfo['extension_description'] = self.section_doc
+            self.metainfo['method'] = method
+            self.metainfo['description'] = kwargs['api_desc']
+            self.metainfo['status'] = response.status
+            self.metainfo['url'] = kwargs['url_doc']
+        return response
+
+    def _convert_url_params(self, params, is_doc=False):
+
+        if not isinstance(params, tuple):
+            params = (params,)
+        if is_doc:
+            return tuple("{" + param + "}" for param in params)
+        else:
+            return params
+
+    def _doc_do_get(self, url, url_api_params, url_doc_params,
+                    strip_version=False, **kwargs):
+        url_api = url % self._convert_url_params(url_api_params)
+        url_doc = url % self._convert_url_params(url_doc_params, is_doc=True)
+        kwargs['url_doc'] = url_doc
+        return self._doc_get_response(url_api, 'GET', None, strip_version,
+                                      **kwargs)
+
+    def _doc_do_post(self, url, url_api_params, url_doc_params, name, subs,
+                     method='POST', **kwargs):
+        url_api = url % self._convert_url_params(url_api_params)
+        url_doc = url % self._convert_url_params(url_doc_params, is_doc=True)
+        kwargs['url_doc'] = url_doc
+        if method in ['POST', 'PUT'] and 'api_desc' in kwargs:
+            self.metainfo['request'] = name + "." + self.ctype
+        body = self._read_template(name) % subs
+        sample = self._get_sample(name)
+        if self.generate_samples and not os.path.exists(sample):
+                self._write_sample(name, body)
+        return self._doc_get_response(url_api, method, body, False, **kwargs)
+
+    def _doc_do_put(self, url, url_api_params, url_doc_params, name, subs,
+                    **kwargs):
+        return self._doc_do_post(url, url_api_params, url_doc_params, name,
+                                 subs, method='PUT', **kwargs)
+
+    def _doc_do_delete(self, url, url_api_params, url_doc_params, **kwargs):
+        url_api = url % self._convert_url_params(url_api_params)
+        url_doc = url % self._convert_url_params(url_doc_params, is_doc=True)
+        kwargs['url_doc'] = url_doc
+        return self._doc_get_response(url_api, 'DELETE', None, False, **kwargs)
+
+    def _write_metafile(self, name, has_response):
+        if self.generate_samples and 'section_name' in self.metainfo:
+            metafile_name = self._get_sample(name) + ".meta"
+            if not os.path.exists(metafile_name):
+                if has_response:
+                    self.metainfo['response'] = name + '.' + self.ctype
+                with open(metafile_name, 'w') as outf:
+                    outf.write(jsonutils.dumps(self.metainfo, sort_keys=True,
+                                               indent=4))
+
+    def _verify_response(self, name, subs, response, exp_code):
+        response = super(ApiSampleTestBaseV3, self)._verify_response(
+            name, subs, response, exp_code)
+        self._write_metafile(name, has_response=True)
+        return response
+
+    def _verify_delete_response(self, name, response, exp_code):
+        self.assertEqual(response.status, exp_code)
+        self._write_metafile(name, has_response=False)
