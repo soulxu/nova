a57dc946779fe5c5dcb94ec0f82b67e1b81cf688
Chris Yeoh <cyeoh@au1.ibm.com>
2013-08-05 16:28:41 +0930

    WIP: Create metafiles for API v3 samples
    Create metafiles for the API v3 samples which can be used to
    help automate the generation of specification documentation for
    the v3 API
    
    Adds the infrastructure for creating the meta files plus adds
    the changes for hosts extension
    
    Partially implements blueprint v3-api-specification
    
    Change-Id: Ie443931ea6bb49fbf2848f2223d0e47436005c8f

--- nova/tests/integrated/v3/test_hosts.py
+++ nova/tests/integrated/v3/test_hosts.py
@@ -19,35 +19,48 @@ from nova.tests.integrated.v3 import api_sample_base
 
 class HostsSampleJsonTest(api_sample_base.ApiSampleTestBaseV3):
     extension_name = "os-hosts"
+    section_name = "Hosts"
+    section_doc = "Manages physical hosts."
 
     def test_host_startup(self):
-        response = self._do_get('os-hosts/%s/startup' % self.compute.host)
+        response = self._doc_do_get(
+            'os-hosts/%s/startup', self.compute.host, 'host_name',
+            api_desc='Starts a host.')
         subs = self._get_regexes()
         self._verify_response('host-get-startup', subs, response, 200)
 
     def test_host_reboot(self):
-        response = self._do_get('os-hosts/%s/reboot' % self.compute.host)
+        response = self._doc_do_get(
+            'os-hosts/%s/reboot', self.compute.host, 'host_name',
+            api_desc='Reboots a host.')
         subs = self._get_regexes()
         self._verify_response('host-get-reboot', subs, response, 200)
 
     def test_host_shutdown(self):
-        response = self._do_get('os-hosts/%s/shutdown' % self.compute.host)
+        response = self._doc_do_get(
+            'os-hosts/%s/shutdown', self.compute.host, 'host_name',
+            api_desc='Shuts down a host.')
         subs = self._get_regexes()
         self._verify_response('host-get-shutdown', subs, response, 200)
 
     def test_host_maintenance(self):
-        response = self._do_put('os-hosts/%s' % self.compute.host,
-                                'host-put-maintenance-req', {})
+        response = self._doc_do_put(
+            'os-hosts/%s', self.compute.host, 'host_name',
+            'host-put-maintenance-req', {},
+            api_desc='Enables a host or puts it in maintenance mode.')
         subs = self._get_regexes()
         self._verify_response('host-put-maintenance-resp', subs, response, 200)
 
     def test_host_get(self):
-        response = self._do_get('os-hosts/%s' % self.compute.host)
+        response = self._doc_do_get(
+            'os-hosts/%s', self.compute.host, 'host_name',
+            api_desc='Shows information for a specified host.')
         subs = self._get_regexes()
         self._verify_response('host-get-resp', subs, response, 200)
 
     def test_hosts_list(self):
-        response = self._do_get('os-hosts')
+        response = self._doc_do_get('os-hosts', (), (),
+                                api_desc='Lists all hosts.')
         subs = self._get_regexes()
         self._verify_response('hosts-list-resp', subs, response, 200)
 
