0ba0d3070ea3ff7961d2f23c049b3294470e8629
He Jie Xu <xuhj@linux.vnet.ibm.com>
2013-09-17 14:45:28 +0800

    Adds os-aggregates metafiles for api samples
    Converts os-aggregates api sample to create meta files for api sample docs.
    We use this to auto generate the specification document
    
    Change-Id: I171b28870f98afe2245ce5a9cc1cbe7163ab4ab7

--- nova/tests/integrated/v3/test_aggregates.py
+++ nova/tests/integrated/v3/test_aggregates.py
@@ -19,33 +19,46 @@ from nova.tests.integrated.v3 import api_sample_base
 
 class AggregatesSampleJsonTest(api_sample_base.ApiSampleTestBaseV3):
     extension_name = "os-aggregates"
+    section_name = 'Aggregates'
+    section_doc = "Admin-only aggregate administration."
 
     def test_aggregate_create(self):
         subs = {
             "aggregate_id": '(?P<id>\d+)'
         }
-        response = self._do_post('os-aggregates', 'aggregate-post-req', subs)
+        response = self._doc_do_post(
+            'os-aggregates', (), (), 'aggregate-post-req', {},
+            api_desc="Creates an aggregate, given its name and "
+                     "availability_zone.")
         subs.update(self._get_regexes())
         return self._verify_response('aggregate-post-resp',
                                      subs, response, 201)
 
     def test_list_aggregates(self):
         self.test_aggregate_create()
-        response = self._do_get('os-aggregates')
+        response = self._doc_do_get(
+            'os-aggregates', (), (),
+            api_desc="Returns a list a host aggregate's id, name, "
+                     "availability_zone.")
         subs = self._get_regexes()
         self._verify_response('aggregates-list-get-resp', subs, response, 200)
 
     def test_aggregate_get(self):
         agg_id = self.test_aggregate_create()
-        response = self._do_get('os-aggregates/%s' % agg_id)
+        response = self._doc_do_get(
+            'os-aggregates/%s', agg_id, 'aggregate_id',
+            api_desc="Shows the details of an aggregate, hosts and metadata "
+                     "included.")
         subs = self._get_regexes()
         self._verify_response('aggregates-get-resp', subs, response, 200)
 
     def test_add_metadata(self):
         agg_id = self.test_aggregate_create()
-        response = self._do_post('os-aggregates/%s/action' % agg_id,
-                                 'aggregate-metadata-post-req',
-                                 {'action': 'set_metadata'})
+        response = self._doc_do_post(
+            'os-aggregates/%s/action', agg_id, 'aggregate_id',
+            'aggregate-metadata-post-req', {'action': 'set_metadata'},
+            api_desc="Add new metadata and replaces the aggregate's existing "
+                     "metadata with new metadata.")
         subs = self._get_regexes()
         self._verify_response('aggregates-metadata-post-resp', subs,
                               response, 200)
@@ -55,8 +68,10 @@ class AggregatesSampleJsonTest(api_sample_base.ApiSampleTestBaseV3):
         subs = {
             "host_name": self.compute.host,
         }
-        response = self._do_post('os-aggregates/%s/action' % aggregate_id,
-                                 'aggregate-add-host-post-req', subs)
+        response = self._doc_do_post(
+            'os-aggregates/%s/action', aggregate_id, 'aggregate_id',
+            'aggregate-add-host-post-req', subs,
+            api_desc="Adds a host to the specified aggregate.")
         subs.update(self._get_regexes())
         self._verify_response('aggregates-add-host-post-resp', subs,
                               response, 202)
@@ -66,16 +81,21 @@ class AggregatesSampleJsonTest(api_sample_base.ApiSampleTestBaseV3):
         subs = {
             "host_name": self.compute.host,
         }
-        response = self._do_post('os-aggregates/1/action',
-                                 'aggregate-remove-host-post-req', subs)
+        response = self._doc_do_post(
+            'os-aggregates/%s/action', 1, 'aggregate_id',
+            'aggregate-remove-host-post-req', subs,
+            api_desc="Removes a host from the specified aggregate.")
         subs.update(self._get_regexes())
         self._verify_response('aggregates-remove-host-post-resp',
                               subs, response, 202)
 
     def test_update_aggregate(self):
         aggregate_id = self.test_aggregate_create()
-        response = self._do_put('os-aggregates/%s' % aggregate_id,
-                                  'aggregate-update-post-req', {})
+        response = self._doc_do_put(
+            'os-aggregates/%s', aggregate_id, 'aggregate_id',
+            'aggregate-update-post-req', {},
+            api_desc="Updates the name and/or availability_zone of "
+                     "given aggregate.")
         subs = self._get_regexes()
         self._verify_response('aggregate-update-post-resp',
                               subs, response, 200)
