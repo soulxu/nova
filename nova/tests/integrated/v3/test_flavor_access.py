d4712d9e5b52984391edf4a5a1a6e708abd3a72f
ivan-zhu <bozhu@linux.vnet.ibm.com>
2013-09-18 17:45:45 +0800

    Adds flavor_access metafiles for api samples
    Converts flavor_access api sample to create meta files for api sample docs.
    We use this to auto generate the specification document
    
    Change-Id: Idd61e4d9bb6f063914dbc40c9bb04dff60d0c567

--- nova/tests/integrated/v3/test_flavor_access.py
+++ nova/tests/integrated/v3/test_flavor_access.py
@@ -19,15 +19,18 @@ from nova.tests.integrated.v3 import api_sample_base
 class FlavorAccessSampleJsonTests(api_sample_base.ApiSampleTestBaseV3):
     extension_name = 'os-flavor-access'
     extra_extensions_to_load = ['flavor-manage']
+    section_name = 'Flavor Access'
+    section_doc = "Flavor access support."
 
     def _add_tenant(self):
         subs = {
             'tenant_id': 'fake_tenant',
             'flavor_id': 10,
         }
-        response = self._do_post('flavors/10/action',
-                                 'flavor-access-add-tenant-req',
-                                 subs)
+        response = self._doc_do_post('flavors/10/action', (), (),
+                                     'flavor-access-add-tenant-req',
+                                     subs,
+                                     api_desc="Add flavor access for tenant.")
         self._verify_response('flavor-access-add-tenant-resp',
                               subs, response, 200)
 
@@ -36,9 +39,12 @@ class FlavorAccessSampleJsonTests(api_sample_base.ApiSampleTestBaseV3):
             'flavor_id': 10,
             'flavor_name': 'test_flavor'
         }
-        response = self._do_post("flavors",
-                                 "flavor-access-create-req",
-                                 subs)
+        response = self._doc_do_post("flavors", (), (),
+                                     "flavor-access-create-req",
+                                     subs,
+                                     api_desc="Extend flavor create to "
+                                     "add accesss attribute to "
+                                     "the response of flavor create.")
         subs.update(self._get_regexes())
         self._verify_response("flavor-access-create-resp", subs, response, 200)
 
@@ -46,7 +52,10 @@ class FlavorAccessSampleJsonTests(api_sample_base.ApiSampleTestBaseV3):
         self._create_flavor()
 
     def test_flavor_access_detail(self):
-        response = self._do_get('flavors/detail')
+        response = self._doc_do_get('flavors/detail', (), (),
+                                    api_desc="Extend flavor detail to "
+                                    "add accesss attribute to "
+                                    "the response of flavor detail.")
         subs = self._get_regexes()
         self._verify_response('flavor-access-detail-resp', subs, response, 200)
 
@@ -54,7 +63,10 @@ class FlavorAccessSampleJsonTests(api_sample_base.ApiSampleTestBaseV3):
         self._create_flavor()
         self._add_tenant()
         flavor_id = 10
-        response = self._do_get('flavors/%s/os-flavor-access' % flavor_id)
+        response = self._doc_do_get('flavors/%s/os-flavor-access' % flavor_id,
+                                    (), (),
+                                    api_desc="Return access list by flavor "
+                                    "id.")
         subs = {
             'flavor_id': flavor_id,
             'tenant_id': 'fake_tenant',
@@ -63,7 +75,10 @@ class FlavorAccessSampleJsonTests(api_sample_base.ApiSampleTestBaseV3):
 
     def test_flavor_access_show(self):
         flavor_id = 1
-        response = self._do_get('flavors/%s' % flavor_id)
+        response = self._doc_do_get('flavors/%s' % flavor_id, (), (),
+                                    api_desc="Extend flavor show to "
+                                    "add accesss attribute to "
+                                    "the response of flavor show.")
         subs = {
             'flavor_id': flavor_id
         }
@@ -80,9 +95,11 @@ class FlavorAccessSampleJsonTests(api_sample_base.ApiSampleTestBaseV3):
         subs = {
             'tenant_id': 'fake_tenant',
         }
-        response = self._do_post('flavors/10/action',
-                                 "flavor-access-remove-tenant-req",
-                                 subs)
+        response = self._doc_do_post('flavors/10/action', (), (),
+                                     "flavor-access-remove-tenant-req",
+                                     subs,
+                                     api_desc="Remove flavor access for "
+                                     "tenant.")
         self._verify_response('flavor-access-remove-tenant-resp',
                               {}, response, 200)
 
